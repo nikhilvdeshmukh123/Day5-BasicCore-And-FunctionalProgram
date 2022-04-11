b175fd15617300d09090774d7a56d866f52e810b
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-04-12 04:23:14 +0530

    Added: Printing The Prime Factors Of Number N

---
+++ src/com/bridgelabz/day5/basicCore/PrimeFactorisation.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.day5.basicCore;
+/*
+ * @author : Nikhil
+ * Print the prime factors of number N.
+ */
+import java.util.Scanner;
+
+public class PrimeFactorisation {
+    public static void main(String[] args) {
+    	//Scanner Class Take Input From User
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the value of n");
+        int n = in.nextInt();
+        System.out.println("The prime factors of " +n +" =");
+        while (n % 2 == 0) {                                // If n is even, print 2. Divide n by 2
+            System.out.print(2 + " ");
+            n = n/2;
+        }
+      //For to get Prime Factor
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {        //If n is odd, i will take values from 3 till squareroot of n, i=i+2 gives odd numbers only
+            while (n % i == 0) {                            //If n is divisible by i
+                System.out.print(i + " ");                  //Print i
+                n = n/i;                                    //Divide n by i
+            }                                               //for loop continues
+        }
+        if (n > 2)                                          //Now if n is greater than 2, it will be a prime number greater than 2
+            System.out.print(n);                            //Print the value of n
+    }
+}
